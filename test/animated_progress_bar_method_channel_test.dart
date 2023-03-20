30ab1ff61f5f361a5dd9a22dbf8c2f13459fc364
Mostafa Efafi <mostafa.ef@gmail.com>
2023-03-20 16:04:30 +0330

    removed junk files

--- test/animated_progress_bar_method_channel_test.dart
+++ test/animated_progress_bar_method_channel_test.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:animated_progress_bar/animated_progress_bar_method_channel.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:animated_progress_bar/animated_progress_bar_method_channel.dart';
 
-void main() {
-  MethodChannelAnimatedProgressBar platform = MethodChannelAnimatedProgressBar();
-  const MethodChannel channel = MethodChannel('animated_progress_bar');
+// void main() {
+//   MethodChannelAnimatedProgressBar platform = MethodChannelAnimatedProgressBar();
+//   const MethodChannel channel = MethodChannel('animated_progress_bar');
 
-  TestWidgetsFlutterBinding.ensureInitialized();
+//   TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
+//   setUp(() {
+//     channel.setMockMethodCallHandler((MethodCall methodCall) async {
+//       return '42';
+//     });
+//   });
 
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
+//   tearDown(() {
+//     channel.setMockMethodCallHandler(null);
+//   });
 
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
-  });
-}
+//   test('getPlatformVersion', () async {
+//     expect(await platform.getPlatformVersion(), '42');
+//   });
+// }
--- test/animated_progress_bar_test.dart
+++ test/animated_progress_bar_test.dart
@@ -1,29 +1,29 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:animated_progress_bar/animated_progress_bar.dart';
-import 'package:animated_progress_bar/animated_progress_bar_platform_interface.dart';
-import 'package:animated_progress_bar/animated_progress_bar_method_channel.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:animated_progress_bar/animated_progress_bar.dart';
+// import 'package:animated_progress_bar/animated_progress_bar_platform_interface.dart';
+// import 'package:animated_progress_bar/animated_progress_bar_method_channel.dart';
+// import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockAnimatedProgressBarPlatform
-    with MockPlatformInterfaceMixin
-    implements AnimatedProgressBarPlatform {
+// class MockAnimatedProgressBarPlatform
+//     with MockPlatformInterfaceMixin
+//     implements AnimatedProgressBarPlatform {
 
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
+//   @override
+//   Future<String?> getPlatformVersion() => Future.value('42');
+// }
 
-void main() {
-  final AnimatedProgressBarPlatform initialPlatform = AnimatedProgressBarPlatform.instance;
+// void main() {
+//   final AnimatedProgressBarPlatform initialPlatform = AnimatedProgressBarPlatform.instance;
 
-  test('$MethodChannelAnimatedProgressBar is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelAnimatedProgressBar>());
-  });
+//   test('$MethodChannelAnimatedProgressBar is the default instance', () {
+//     expect(initialPlatform, isInstanceOf<MethodChannelAnimatedProgressBar>());
+//   });
 
-  test('getPlatformVersion', () async {
-    AnimatedProgressBar animatedProgressBarPlugin = AnimatedProgressBar();
-    MockAnimatedProgressBarPlatform fakePlatform = MockAnimatedProgressBarPlatform();
-    AnimatedProgressBarPlatform.instance = fakePlatform;
+//   test('getPlatformVersion', () async {
+//     AnimatedProgressBar animatedProgressBarPlugin = AnimatedProgressBar();
+//     MockAnimatedProgressBarPlatform fakePlatform = MockAnimatedProgressBarPlatform();
+//     AnimatedProgressBarPlatform.instance = fakePlatform;
 
-    expect(await animatedProgressBarPlugin.getPlatformVersion(), '42');
-  });
-}
+//     expect(await animatedProgressBarPlugin.getPlatformVersion(), '42');
+//   });
+// }
